8497a6108d4f266b42ee22575944636b65c53cad
Tai Vinh <tai.vinhdn05@gmail.com>
2026-04-14 20:20:41 +0200

    Angular Route Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { UsersComponent } from './features/users/users';
+import { authGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,6 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+    canActivate: [authGuard],
   },
 ];
